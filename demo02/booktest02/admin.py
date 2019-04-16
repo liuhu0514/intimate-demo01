9627977da5407a24d859723dd8f5327a664e555a
lh <1466947051@qq.com>
2019-04-16 21:46:37 +0800

    创建数据迁移

--- demo02/booktest02/admin.py
+++ demo02/booktest02/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import TeaInfo, ClassInfo, StuInfo
 
 # Register your models here.
+
+
+class ClassInfoInline(admin.StackedInline):
+    model = ClassInfo
+    # 关联个数
+    extra = 1
+
+
+class StuInfoInline(admin.StackedInline):
+    model = StuInfo
+    extra = 1
+
+
+class TeaInfoAdmin(admin.ModelAdmin):
+    # 显示字段，可以点击列头进行排序
+    list_display = ['id1', 'skill', 'tsub']
+    # 过滤字段，过滤边框会出现在右侧
+    list_filter = ['tname']
+    # 搜索字段。搜索框会出现在上侧
+    search_fields = ['tname', 'tsubject']
+    # 分页，分页框会出现在下侧
+    list_per_page = 5
+    # 添加教师的时候可以额外添加班级
+    inlines = [ClassInfoInline]
+
+
+class ClassInfoAdmin(admin.ModelAdmin):
+    list_display = ['id1', 'skill', 'tea']
+    list_filter = ['cname']
+    search_fields = ['cname']
+    list_per_page = 5
+    inlines = [StuInfoInline]
+
+
+class StuInfoAdmin(admin.ModelAdmin):
+    list_display = ['id1', 'skill', 'age', 'gender', 'sc']
+    list_filter = ['sname']
+    search_fields = ['sname', 'sage']
+    list_per_page = 5
+
+
+admin.site.register(TeaInfo, TeaInfoAdmin)
+admin.site.register(ClassInfo, ClassInfoAdmin)
+admin.site.register(StuInfo, StuInfoAdmin)
--- demo02/booktest02/models.py
+++ demo02/booktest02/models.py
@@ -7,6 +7,18 @@ class TeaInfo(models.Model):
     tname = models.CharField(max_length=20)
     tsubject = models.CharField(max_length=20)
 
+    def skill(self):
+        return self.tname
+    skill.short_description = '名字'
+
+    def tsub(self):
+        return self.tsubject
+    tsub.short_description = '课程'
+
+    def id1(self):
+        return self.pk
+    id1.short_description = '编号'
+
     def __str__(self):
         return self.tname
 
@@ -18,6 +30,18 @@ class ClassInfo(models.Model):
     def __str__(self):
         return self.cname
 
+    def id1(self):
+        return self.pk
+    id1.short_description = '编号'
+
+    def skill(self):
+        return self.cname
+    skill.short_description = '班级名'
+
+    def tea(self):
+        return self.ctea
+    tea.short_description = '教师'
+
 
 class StuInfo(models.Model):
     sname = models.CharField(max_length=20)
@@ -28,4 +52,24 @@ class StuInfo(models.Model):
 
     def __str__(self):
         return self.sname
+
+    def id1(self):
+        return self.pk
+    id1.short_description = '编号'
+
+    def skill(self):
+        return self.sname
+    skill.short_description = '名字'
+
+    def age(self):
+        return self.sage
+    age.short_description = '年龄'
+
+    def gender(self):
+        return self.sgender
+    gender.short_description = '性别'
+
+    def sc(self):
+        return self.sclass
+    sc.short_description = '班级'
 
