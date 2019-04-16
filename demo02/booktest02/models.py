3c11f93ff4e60f75bd30fa3efa094d696ca29951
lh <1466947051@qq.com>
2019-04-16 19:32:11 +0800

    生成迁移文件，执行迁移

--- demo02/booktest02/models.py
+++ demo02/booktest02/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TeaInfo(models.Model):
+    tname = models.CharField(max_length=20)
+    tsubject = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.tname
+
+
+class ClassInfo(models.Model):
+    cname = models.CharField(max_length=20)
+    ctea = models.ForeignKey('TeaInfo', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.cname
+
+
+class StuInfo(models.Model):
+    sname = models.CharField(max_length=20)
+    sage = models.IntegerField()
+    sgender = models.BooleanField()
+    # 外键
+    sclass = models.ForeignKey('ClassInfo', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.sname
+
