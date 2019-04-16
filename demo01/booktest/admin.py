c50c2a27906871a47bb816f41844c0cbc4cc836d
lh <1466947051@qq.com>
2019-04-16 16:39:51 +0800

    添加管理员，后台管理功能，语言设置

--- demo01/booktest/admin.py
+++ demo01/booktest/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-
+from .models import BookInfo, HeroInfo
 # Register your models here.
-
+admin.site.register(BookInfo)
+admin.site.register(HeroInfo)
 
 """
 通过少量的代码实现强大的后台管理 
--- demo01/booktest/models.py
+++ demo01/booktest/models.py
@@ -7,6 +7,9 @@ class BookInfo(models.Model):
     btitle = models.CharField(max_length=20)
     bpub_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.btitle
+
 
 class HeroInfo(models.Model):
     hname = models.CharField(max_length=20)
@@ -14,6 +17,9 @@ class HeroInfo(models.Model):
     hcontent = models.CharField(max_length=50)
     # 外键 第一个参数为表名  第二个参数代表删除类型
     hbook = models.ForeignKey('BookInfo', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.hname
 
 
 """
