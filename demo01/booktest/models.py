eb06d275f3f48542c49f15c22022480404ad26be
lh <1466947051@qq.com>
2019-04-16 11:54:18 +0800

    数据迁移，及生成数据迁移

--- demo01/booktest/models.py
+++ demo01/booktest/models.py
@@ -3,9 +3,22 @@ from django.db import models
 # Create your models here.
 
 
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=20)
+    bpub_date = models.DateTimeField(auto_now_add=True)
+
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField()
+    hcontent = models.CharField(max_length=50)
+    # 外键 第一个参数为表名  第二个参数代表删除类型
+    hbook = models.ForeignKey('BookInfo', on_delete=models.CASCADE)
+
+
 """
 django MVT M
-ORM 对象中的 O 
+ORM 对象中的 O
 需要定义实体类
 """
 
