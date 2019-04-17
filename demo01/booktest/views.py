2701fe820a550a6c81775d067165050ff38e0441
lh <1466947051@qq.com>
2019-04-17 12:00:12 +0800

    视图urls绑定

--- demo01/booktest/views.py
+++ demo01/booktest/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('首页')
+
+
+def list(request):
+    return HttpResponse('列表页')
+
 
 """
 视图函数
