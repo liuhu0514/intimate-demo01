fc4ddc34e50220cb8d5a30160720a48aa1824338
lh <1466947051@qq.com>
2019-04-17 17:33:04 +0800

    页面跳转读取数据库信息

--- demo01/booktest/urls.py
+++ demo01/booktest/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'$', views.index),
-    url(r'list/$', views.list),
+    url(r'^$', views.index),
+    url(r'^list/$', views.list),
+    url(r'^detail/(\d+)/$', views.detail),
+    url(r'^herodetail/(\d+)/$', views.herodetail),
 ]
--- demo01/booktest/views.py
+++ demo01/booktest/views.py
@@ -1,15 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import HeroInfo, BookInfo
+from django.template import loader
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('首页')
+    # return HttpResponse('首页')
+    # 加载模板
+    temp = loader.get_template('booktest/index.html')
+    con = {'username': '希特勒'}
+    temp = temp.render(con)
+    return HttpResponse(temp)
 
 
 def list(request):
-    return HttpResponse('列表页')
+    bs = BookInfo.objects.all()
+    temp = loader.get_template('booktest/list.html')
+    con = {'booklist': bs}
+    result = temp.render(con)
+    return render(request, 'booktest/list.html', context=con)
+    # return HttpResponse(result)
+
+
+def detail(request,id):
+    try:
+        b = BookInfo.objects.get(pk=int(id))
+        temp = loader.get_template('booktest/detail.html')
+        con = {'book': b}
+        result = temp.render(con)
+        return HttpResponse(result)
+    except Exception as e:
+        print(e)
+
+
+def herodetail(request,id):
+    hero = HeroInfo.objects.get(pk=id)
+    temp = loader.get_template('booktest/herodetail.html')
+    con = {'hero': hero}
+    temp = temp.render(con)
+    return HttpResponse(temp)
 
 
 """
